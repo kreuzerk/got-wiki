97421b6648df8825b097fe4e2b499f3887c51f65
kreuzerk <kevin.kreuzer90@icloud.com>
2020-04-11 15:53:17 +0200

    feat(snow): add john snow picture

--- projects/got-wiki/src/lib/john-snow/john-snow.component.ts
+++ projects/got-wiki/src/lib/john-snow/john-snow.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'got-wiki-john-snow',
-  templateUrl: './john-snow.component.html'
+  templateUrl: './john-snow.component.html',
+  styleUrls: ['./john-snow.component.scss', '../characters.scss']
 })
 export class GotWikiJohnSnowComponent {
 }
