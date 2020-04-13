ebba59f0011e623e000192e9f7bedd777ded33cd
kreuzerk <kevin.kreuzer90@icloud.com>
2020-04-13 21:18:37 +0200

    chore(lib): export all componenents

--- projects/got-wiki/src/public-api.ts
+++ projects/got-wiki/src/public-api.ts
@@ -3,3 +3,30 @@
  */
 export * from './lib/john-snow/john-snow.module';
 export * from './lib/john-snow/john-snow.component';
+
+export * from './lib/arya-stark/arya-stark.module';
+export * from './lib/arya-stark/arya-stark.component';
+
+export * from './lib/bran-stark/bran-stark.component';
+export * from './lib/bran-stark/bran-stark.module';
+
+export * from './lib/robb-stark/robb-stark.module';
+export * from './lib/robb-stark/robb-stark.component';
+
+export * from './lib/cersei-lannister/cersei-lannister.module';
+export * from './lib/cersei-lannister/cersei-lannister.component';
+
+export * from './lib/jaime-lannister/jaime-lannister.module';
+export * from './lib/jaime-lannister/jaime-lannister.component';
+
+export * from './lib/tyrion-lannister/tyrion-lannister.module';
+export * from './lib/tyrion-lannister/tyrion-lannister.component';
+
+export * from './lib/tywin-lannister/tywin-lannister.module';
+export * from './lib/tywin-lannister/tywin-lannister.component';
+
+export * from './lib/starks/starks.module';
+export * from './lib/starks/starks.component';
+
+export * from './lib/lannisters/lannisters.module';
+export * from './lib/lannisters/lannisters.component';
